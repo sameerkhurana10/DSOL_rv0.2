e295d63f69d2f9e32c238c44965970df6f4f0160
Sameer Khurana <sameerkhurana10@gmail.com>
2018-01-16 06:47:07 -0500

    decode added

--- run.sh
+++ run.sh
@@ -5,6 +5,8 @@ parameter_setting_id="DeepSol1"
 keras_backend=theano
 cuda_root='/usr/local/cuda'
 device=cpu
+model=
+mode=
 
 . parse_options.sh || exit 1;
 
@@ -13,6 +15,8 @@ if [ $# != 1 ]; then
     echo "usage: ./run.sh <data-dir>"
     echo "e.g.:  ./run.sh data/protein_dsol1.data"
     echo "main options (for others, see top of script file)"
+    echo "  --model                                            # default full."
+    echo "  --mode                                             # default full."
     echo "  --conf_file                                        # default full."
     echo "  --parameter_setting_id                             # default full."
     echo "  --keras_backend                                    # use graphs in src-dir"
@@ -23,7 +27,7 @@ fi
 
 data=$1
 
-if [[ "$device" == 'cpu' ]] ; then
+if [[ $device == 'cpu' ]] ; then
     export THEANO_FLAGS="base_compiledir=$(pwd)/.theano,device=${device},floatX=float32"
 fi
 
@@ -33,10 +37,17 @@ if [[ $device == *"cuda"* ]]; then
     export THEANO_FLAGS="base_compiledir=$(pwd)/.theano,cuda.root=${cuda_root},device=${device},floatX=float32"
 fi
 
+if [[ $mode == 'train' ]] ; then
+    if [[ $model == "deepsol1" ]]; then
+	KERAS_BACKEND=${keras_backend} python main_dsol1.py -conf_file ${conf_file} -parameter_setting_id ${parameter_setting_id} -data ${data}
+    fi
+fi
 
-export KERAS_BACKEND=${keras_backend} 
-
-./main_dsol1.py -conf_file ${conf_file} -parameter_setting_id ${parameter_setting_id} -data ${data}
+if [[ $mode == 'decode' ]] ; then
+    if [[ $model == "deepsol1" ]]; then
+	KERAS_BACKEND=${keras_backend} python main_dsol1.py -conf_file ${conf_file} -parameter_setting_id ${parameter_setting_id} -data ${data}
+    fi
+fi
 
 # for running DSOL2
 #python main_dsol2.py -data data/protein_dsol2.data -conf_file parameters.json -parameter_setting_id DeepSol2
